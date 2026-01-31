e1e92e3e175a726172a8f7e9c1b0d7193020c1c6
Exahel Castaneda <exahelcastaneda@gmail.com>
2026-01-31 00:25:40 -0500

    first version

--- movies/views.py
+++ movies/views.py
@@ -55,7 +55,10 @@ def edit_review(request, id, review_id):
         return redirect('movies.show', id=id)
 @login_required
 def delete_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id, user=request.user)
+    if request.user.is_staff: 
+        review = get_object_or_404(Review, id=review_id)
+    else:
+        review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
 @login_required
